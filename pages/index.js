37044a0d5b7b456e1bed92edfe8b9c1147d2eec7
Obafemi Lawal <swissbobo@gmail.com>
2022-07-05 22:19:55 +0300

    used getStaticProps and getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from 'components/layout/Layout';
 import utilStyles from 'styles/utils.module.scss';
+import { getSortedPostsData } from 'lib/posts'; /* this line gives error if not 
+  called inside next functions like e.g getStaticProps */
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +17,33 @@ export default function Home() {
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+/* By returning allPostsData inside the props object in getStaticProps, 
+  the blog posts will be passed to the Home component as a prop. 
+  Now you can access the blog posts like so: */
